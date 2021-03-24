55f58b07a13a1a881529a9de120d51336c3efae9
Julia Bergman <julia.bergman@helsinki.fi>
2021-03-24 22:24:46 +0200

    Viikko 3 teht 8

--- viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -1,5 +1,6 @@
 package ohtu;
 
+import java.util.Random;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Tester {
 
     public static void main(String[] args) {
+        /* alkuperäinen
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://localhost:4567");
@@ -30,12 +32,93 @@ public class Tester {
 
         sleep(3);
         
+        driver.quit(); 
+         */
+
+ /* epäonnistunut kirjautuminen: oikea käyttäjätunnus, väärä salasana
+    
+    
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://localhost:4567");
+        
+        sleep(2);
+        
+        WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akkep123");
+        element = driver.findElement(By.name("login"));
+        
+        sleep(2);
+        element.submit();
+
+        sleep(3);
+        
+        driver.quit();
+         */
+        //uuden käyttäjätunnuksen luominen ja uloskirjautuminen
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://localhost:4567");
+
+        sleep(2);
+
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        sleep(2);
+
+        Random r = new Random();
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("susanna" + r.nextInt(100000));
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("annasus");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("annasus");
+        element = driver.findElement(By.name("signup"));
+
+        sleep(2);
+        element.submit();
+
+        sleep(3);
+        
+        WebElement element2 = driver.findElement(By.linkText("continue to application mainpage"));
+        element2.click();
+        
+        sleep(5);
+        
+        //StateElementreferenceException tuli usein joten
+        clickLinkWithText("logout", driver);
+
+        sleep(5);
+        
         driver.quit();
     }
+
+    private static void sleep(int n) {
+        try {
+            Thread.sleep(n * 1000);
+        } catch (Exception e) {
+        }
+    }
     
-    private static void sleep(int n){
-        try{
-            Thread.sleep(n*1000);
-        } catch(Exception e){}
+    private static void clickLinkWithText(String text, WebDriver driver) {
+        int trials = 0;
+        while( trials++<5 ) {
+            try{
+                WebElement element = driver.findElement(By.linkText(text));
+                element.click();
+                break;           
+            } catch(Exception e) {
+                System.out.println(e.getStackTrace());
+            }
+        }
     }
 }
