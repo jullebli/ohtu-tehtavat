9869d2f0a9f04dc22341619bcc4402e0c91e1173
Julia Bergman <julia.bergman@helsinki.fi>
2021-04-20 15:38:59 +0300

    Viikko 6 teht 3

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -8,7 +8,7 @@ public class Main {
         String url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt";
 
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
-/*
+        /*
         Matcher m = new And(new HasAtLeast(5, "goals"),
                 new HasAtLeast(5, "assists"),
                 new PlaysIn("PHI")
@@ -17,7 +17,7 @@ public class Main {
         for (Player player : stats.matches(m)) {
             System.out.println(player);
         }
-*/
+
         Matcher ma = new And(
                 new Not(new HasAtLeast(1, "goals")),
                 new PlaysIn("NYR")
@@ -39,5 +39,28 @@ public class Main {
         }
         
         System.out.println(stats.matches(new All()).size());
+         
+
+        Matcher matc = new Or(new HasAtLeast(40, "goals"),
+                new HasAtLeast(60, "assists")
+        );
+
+        for (Player player : stats.matches(matc)) {
+            System.out.println(player);
+        }
+        */
+
+        Matcher match = new And(
+                new HasAtLeast(50, "points"),
+                new Or(
+                        new PlaysIn("NYR"),
+                        new PlaysIn("NYI"),
+                        new PlaysIn("BOS")
+                )
+        );
+        
+        for (Player player : stats.matches(match)) {
+            System.out.println(player);
+        }
     }
 }
