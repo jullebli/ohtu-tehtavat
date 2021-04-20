4e9a4d16210b331cf8ad00be9b069c9523f0d0fb
Julia Bergman <julia.bergman@helsinki.fi>
2021-04-20 14:54:43 +0300

    Viikko 6 Teht 2

---
+++ viikko6/QueryLanguage/src/main/java/statistics/matcher/Not.java
@@ -0,0 +1,25 @@
+
+package statistics.matcher;
+
+import statistics.Player;
+import statistics.matcher.Matcher;
+
+public class Not implements Matcher {
+
+    private Matcher[] matchers;
+
+    public Not(Matcher... matchers) {
+        this.matchers = matchers;
+    }
+
+    @Override
+    public boolean matches(Player p) {
+        for (Matcher matcher : matchers) {
+            if (matcher.matches(p)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
